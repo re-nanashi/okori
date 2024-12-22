e8dc2197c9672e5c80e2dcf5a4d4de79e1865cf2
re-nanashi <jfabro3124@gmail.com>
2024-12-22 21:11:48 +0800

    feat: Init springboot application

---
+++ backend/src/main/java/com/okori/workout_tracker_api/WorkoutTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.okori.workout_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutTrackerApiApplication.class, args);
+	}
+
+}
