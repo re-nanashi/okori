639b714331358109f837d3252aa2f180ef88bb21
re-nanashi <jfabro3124@gmail.com>
2025-02-08 21:21:11 +0800

    chore: Remove unnecessary comments

--- backend/src/main/java/com/okori/workout_tracker_api/entity/User.java
+++ backend/src/main/java/com/okori/workout_tracker_api/entity/User.java
@@ -6,28 +6,6 @@ import org.hibernate.annotations.NaturalId;
 import java.util.ArrayList;
 import java.util.List;
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-//     private String firstName;
-//     private String lastName;
-//     @NaturalId
-//     private String email;
-//     private String password;
-// 
-//     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-//     private Cart cart;
-// 
-//     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-//     private List<Order> orders;
-// 
-//     @ManyToMany(fetch = FetchType.EAGER, cascade =
-//             {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-//     @JoinTable(name = "user_roles",  joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-//             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
-//     )
-//     private Collection<Role> roles = new HashSet<>();
-
 @Entity
 public class User {
     @Id
